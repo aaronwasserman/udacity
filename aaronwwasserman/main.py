ec1b01e61fccae31ae1e68efb70ed768e3683743
Aaron Wasserman <aaron.wasserman@blackboard.com>
2013-07-22 12:06:28 -0700

    Optimizations after import into IDE

--- aaronwwasserman/main.py
+++ aaronwwasserman/main.py
@@ -5,15 +5,11 @@ import jinja2
 import blogutils
 import blogmodels
 
-from google.appengine.ext import db
 from google.appengine.api import memcache
-from datetime import timedelta
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = True)
-
-
+jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
+							   autoescape=True)
 
 
 class Handler(webapp2.RequestHandler):
@@ -38,7 +34,7 @@ class Handler(webapp2.RequestHandler):
 	def check_secure_cookie(self):
 		user_cookie = self.request.cookies.get('user_id')
 		cookie_secure = True
-		
+
 		if user_cookie:
 			username, uid = self.parse_cookie(user_cookie)
 			user = blogmodels.User.get_user(username)
@@ -47,8 +43,8 @@ class Handler(webapp2.RequestHandler):
 			elif not int(uid) == user.key().id():
 				cookie_secure = False
 
-		return cookie_secure					
-		
+		return cookie_secure
+
 	def make_secure_cookie(self, user):
 		username = str(user.username)
 		uid = str(user.key().id())
@@ -56,33 +52,34 @@ class Handler(webapp2.RequestHandler):
 
 	def get_user_inputs(self, case = ''):
 		if case == 'signup' or case == 'login':
-			user_inputs = {'username' : self.fetch('username'), 'password' : 
+			user_inputs = {'username' : self.fetch('username'), 'password' :
 							self.fetch('password')}
 			if case == 'signup':
 				user_inputs['verify'] = self.fetch('verify')
 				user_inputs['email'] = self.fetch('email')
-		
+
 		elif case == 'newpost':
-			user_inputs = {'subject' : self.fetch('subject'), 
+			user_inputs = {'subject' : self.fetch('subject'),
 							'content' : self.fetch('content')}
-		
+
 		else:
 			return None
 
 		return user_inputs
 
+
 	def get_input_errors(self, inputs, case = ''):
 		errors_exist = False
 		errors = inputs
-		
+
 		if case == 'signup':
 			username = inputs.get('username')
 			password = inputs.get('password')
 			verify = inputs.get('verify')
 			email = inputs.get('email')
-			
+
 			re_check = blogutils.regexChecking()
-						
+
 			if not re_check.username(username):
 				errors['username_error'] = 'Invalid username.'
 				errors_exist = True
@@ -98,25 +95,25 @@ class Handler(webapp2.RequestHandler):
 			if email and not re_check.email(email):
 				errors['email_error'] = "Invalid email address."
 				errors_exist = True
-		
+
 		elif case == 'login':
 			username = inputs.get('username')
 			password = inputs.get('password')
 			user = blogmodels.User.get_user(username)
-			
+
 			if not user or not user.check_pass(user, password):
 				errors['login_error'] = 'Invalid credentials. Please try again.'
 				errors_exist = True
-		
+
 		elif case == 'newpost':
 			subject = inputs.get('subject')
 			content = inputs.get('content')
-			
+
 			if not subject or not content:
 				errors['newpost_error'] = 'Please provide both subject and content.'
 				errors_exist = True
 
-		return errors if errors_exist else None	
+		return errors if errors_exist else None
 
 	def initialize(self, *a, **kw):
 		webapp2.RequestHandler.initialize(self, *a, **kw)
@@ -141,7 +138,7 @@ class MainPage(Handler):
 					self.write(blog_json)
 				else:
 					self.render('blog_home.html', front_page = [blog], cache_age = blog.cache_age)
-			
+
 			else: # Blog passed in URL was not valid (or no longer found within the db)
 				self.redirect('/')
 
@@ -186,7 +183,7 @@ class Login(Handler):
 		if not errors:
 			user = blogmodels.User.get_user(inputs.get('username'))
 			self.make_secure_cookie(user)
-			self.redirect('/welcome/')
+			self.redirect('/welcome/%s' % user.username)
 		else:
 			self.render('blog_login.html', **errors)
 
@@ -202,28 +199,28 @@ class Logout(Handler):
 
 
 class Welcome(Handler):
-	def get(self):
-		self.render('blog_welcome.html')
+	def get(self, username):
+		self.render('blog_welcome.html', username = username)
 
 
 
 
 class NewPost(Handler):
-    def get(self):
-    	self.render('blog_newpost.html')
-    	
-    
-    def post(self):
-        inputs = self.get_user_inputs('newpost')
-        errors = self.get_input_errors(inputs, 'newpost')
-        
-        if not errors:
-            new_blog_id = blogmodels.Blog.create(inputs)
-            self.redirect('/%s' % new_blog_id)
-        else:
-            self.render('blog_newpost.html', **errors)
-            
-            				
+	def get(self):
+		self.render('blog_newpost.html')
+
+
+	def post(self):
+		inputs = self.get_user_inputs('newpost')
+		errors = self.get_input_errors(inputs, 'newpost')
+
+		if not errors:
+			new_blog_id = blogmodels.Blog.create(inputs)
+			self.redirect('/%s' % new_blog_id)
+		else:
+			self.render('blog_newpost.html', **errors)
+
+
 
 
 class Flush(Handler):
@@ -236,15 +233,15 @@ class Flush(Handler):
 			self.redirect('/')
 
 
-		
+
 
 
 app = webapp2.WSGIApplication([
-    ('/(\d+)?(/?json)?/?', MainPage),
-    ('/signup/?', Signup),
-    ('/login/?', Login),
-    ('/logout/?', Logout),
-	('/welcome/?', Welcome),    
-    ('/newpost/?', NewPost),
-    ('/?(\d+)?(?:/flush)/?', Flush)
+	('/(\d+)?(/?json)?/?', MainPage),
+	('/signup/?', Signup),
+	('/login/?', Login),
+	('/logout/?', Logout),
+	('/welcome/(.+)/?', Welcome),
+	('/newpost/?', NewPost),
+	('/?(\d+)?(?:/flush)/?', Flush)
 ], debug=True)
